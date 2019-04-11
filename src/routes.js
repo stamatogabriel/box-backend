376d7a578d36d7b7c09c76e4dd1017831534dbf6
Gabriel Henrique Stamato <stamato7@gmail.com>
2019-04-11 12:41:46 -0300

    correção de erros

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ const FileController = require('./controllers/FileController');
 routes.post('/boxes', BoxController.store);
 routes.get('/boxes/:id', BoxController.show);
 
-routes.post('boxes/:id/files', multer(multerConfig).single('file'), FileController.store); //o metodo .single permite o envio de um arquivo por vez, para vários, pode-se usar o .array
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store); //o metodo .single permite o envio de um arquivo por vez, para vários, pode-se usar o .array
 
 
 
