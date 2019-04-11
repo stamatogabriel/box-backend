73193e999bf948e9e28767a59990b82dca62215c
Gabriel Henrique Stamato <stamato7@gmail.com>
2019-04-11 13:05:01 -0300

    correção de erros

--- src/models/Files.js
+++ src/models/Files.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const File = new mongoose.Schema({ 
+const File = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    path:{
-        type: String,
-        required: true,
-    },
- }, {
-     timestamps: true,
-     toObject: true, //carrega o File.virtual
-     toJSON: true
- });
+    path: {
+      type: String,
+      require: true
+    }
+  },
+  {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  }
+);
 
- File.virtual('url').get(function(){
-     const url = process.env.URL || 'http://localhost:3333';
-     return `${url}/files/${encodeURIComponent(this.path)}`//caminho virtual para acessar os arquivos
- })
+File.virtual("url").get(function() {
+  const url = process.env.URL || "http://localhost:3333";
 
- module.exports = mongoose.model('File', File);
+  return `${url}/files/${encodeURIComponent(this.path)}`;
+});
+
+module.exports = mongoose.model("File", File);
